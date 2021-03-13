5af401d7ef7a933cdff1fefb0e333f21c85612fb
dongfangxu <dongfangxu2015@outlook.com>
2021-03-13 22:42:55 +0800

    #week08作业提交

---
+++ Week_08/class15/database-split/src/main/java/com/example/databasesplit/DatabaseSplitApplication.java
@@ -0,0 +1,13 @@
+package com.example.databasesplit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseSplitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatabaseSplitApplication.class, args);
+    }
+
+}
